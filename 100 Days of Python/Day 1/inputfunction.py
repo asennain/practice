31ec2d8171984dc5f49c0b59c61b7a5445ec1feb
asennain <asennaini@gmail.com>
2023-02-10 01:32:05 -0800

    Minor updates and some files created

---
+++ 100 Days of Python/Day 1/inputfunction.py
@@ -0,0 +1,2 @@
+# input allows you to put the prompt inside 
+input("What's you favorite food?")
--- matrix_multiply.py
+++ matrix_multiply.py
@@ -1,4 +1,4 @@
-def the_best(A = [], B = []):
+def the_best(A):  # sourcery skip: default-mutable-arg
     # return number of rows and columns for both matrices as string 
     return A
     
