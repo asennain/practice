9d39cef14f34f24f949f092b1fb488a80ed6f84e
Abdul Sennain <asennaini@gmail.com>
2023-02-27 04:20:19 -0800

    rearranged folder structure

--- 100_Days_of_Python/Day1/bandnamegenerator.py
+++ 100_Days_of_Python/Day1/bandnamegenerator.py
@@ -1,3 +1,3 @@
 city = input("What's the name of the city you grew up in?")
 pet = input("What's your last pet's name?")
-print(f"Your band name is {city} {pet}")
+print(f"Your band name is {city} {pet}")    
